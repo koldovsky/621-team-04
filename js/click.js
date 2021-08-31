f4717e5c5837357b91764ec397eaed02d5477148
VasyaPikh <pikhvasya@gmail.com>
2021-08-31 10:09:09 +0000

    Add random clicker

--- js/click.js
+++ js/click.js
@@ -1,5 +1,11 @@
-const button = document.querySelector('button');
+/*const button = document.querySelector('button');
 
 button.addEventListener('click', event => {
   button.textContent = `Click count: ${event.detail}`;
 });
+
+*/ 
+
+function randomSum(min, max) {
+  document.querySelector('.sum').innerHTML = (Math.floor(Math.random() * (max - min + 1) ) + min)
+}
