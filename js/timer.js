e3bf260333ccd4fb7cd814a0a44f43e357c5b6a6
VasyaPikh <pikhvasya@gmail.com>
2021-08-31 11:48:13 +0000

    work timer

--- js/timer.js
+++ js/timer.js
@@ -9,7 +9,7 @@ const startWatch = () => {
 		milliseconds += 10;
 		let dateTimer = new Date(milliseconds);
 		watch.innerHTML =
-			('0'+dateTimer.getUTCHours()).slice(-2) + ':' +
+			
 			('0'+dateTimer.getUTCMinutes()).slice(-2) + ':' +
 			('0'+dateTimer.getUTCSeconds()).slice(-2) + ':' +
 			('0'+dateTimer.getUTCMilliseconds()).slice(-3,-1);
